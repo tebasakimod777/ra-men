bd3309654b107efeef4b19a2115138ff8ec74b0b
s55224kt <koutarou1276@gmail.com>
2023-06-14 20:55:39 +0900

    NotificationTaskクラスを作成。MainActivityで通知のテストの処理を追加

--- android/app/src/main/java/com/example/keyminder/MainActivity.java
+++ android/app/src/main/java/com/example/keyminder/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.keyminder;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static androidx.core.content.ContextCompat.getSystemService;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Activity;
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NotificationTask task = new NotificationTask(this);
+
+        Button testButton = findViewById(R.id.test_button);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String channelId = "channel_id";
+                String title = "通知";
+                String message = "通知をお知らせします";
+                task.sendNotification(channelId, title, message);
+            }
+        });
+
     }
 }
