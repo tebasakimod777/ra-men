08dad5b998021e1bfaaa667591807cb50b25989f
nomke <keita.nomura.228@gmail.com>
2023-06-17 12:06:23 +0900

    WIP(ファイルの命名規則がおかしいとか出て、解決できないので一度ブランチを新しくする)

--- android/app/src/main/java/com/example/keyminder/MainActivity.java
+++ android/app/src/main/java/com/example/keyminder/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.keyminder;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button orientationActivitySwitchButton = (Button) findViewById(R.id.wifi_check_btn);
+        orientationActivitySwitchButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent intent = new Intent(getApplication(), WifiCheck.class);
+                startActivity(intent);
+            }
+        });
     }
 }
