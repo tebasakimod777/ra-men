a865ac00aaf2b5c74d8a01e9e324fc0e07a12650
nomke <keita.nomura.228@gmail.com>
2023-06-17 11:31:44 +0900

    WIP(Intent化を実行する手前、テストまだ)

--- android/app/src/main/java/com/example/keyminder/WifiCheck.java
+++ android/app/src/main/java/com/example/keyminder/WifiCheck.java
@@ -1,4 +1,51 @@
 package com.example.keyminder;
 
-public class WifiCheck {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+public class WifiCheck extends AppCompatActivity {
+    private Button btnGetIPAddress;
+    private TextView txtIPAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wifi_registration);
+
+        btnGetIPAddress = findViewById(R.id.btnGetIPAddress);
+        txtIPAddress = findViewById(R.id.txtIPAddress);
+
+        btnGetIPAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ipAddress = getIPAddress();
+                txtIPAddress.setText(ipAddress);
+            }
+        });
+    }
+
+    private String getIPAddress() {
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = interfaces.nextElement();
+                Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    InetAddress address = addresses.nextElement();
+                    if (!address.isLoopbackAddress() && address.getAddress().length == 4) {
+                        return address.getHostAddress();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
